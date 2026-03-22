13c489db74c33637bf5305f67ad79e3156989403
Jakub Pomykała <jakub.pomykala@gmail.com>
2026-03-22 15:29:01 +0100

    Add privacy policy

--- website/src/main.js
+++ website/src/main.js
@@ -1,1 +1,6 @@
 import './style.css'
+import { renderNav } from './components/nav.js'
+import { renderFooter } from './components/footer.js'
+
+renderNav()
+renderFooter()
--- website/vite.config.js
+++ website/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: './',
   build: {
     outDir: '../docs',
-  }
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        'privacy-policy': resolve(__dirname, 'privacy-policy.html'),
+      },
+    },
+  },
 })
